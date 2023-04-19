2ccbb0709cc4cfc9da4f008eaecca82f37c144bc
Elnatan <elnatanmesfintesfa20@augustana.edu>
2023-04-19 00:38:09 -0500

    Updated the skills and fixed some bugs

--- script.js
+++ script.js
@@ -23,48 +23,40 @@ function changeActivePosition(activeItem){
     activeItem.classList.add('active');
 };
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-
-const track = document.getElementById("image-track");
-
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-const handleOnUp = () => {
-track.dataset.mouseDownAt = "0";  
-track.dataset.prevPercentage = track.dataset.percentage;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-const handleOnMove = e => {
-if(track.dataset.mouseDownAt === "0") return;
-
-const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-
-const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-track.dataset.percentage = nextPercentage;
-
-track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-}, { duration: 1200, fill: "forwards" });
-
-for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-    objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+function showSlides(n) {
+    const slides = document.querySelectorAll('.slides img');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('active', 'prev', 'next');
+    }
+    slides[slideIndex - 1].classList.add('active');
+    if (slideIndex > 1) {
+      slides[slideIndex - 2].classList.add('prev');
+    } else {
+      slides[slides.length - 1].classList.add('prev');
+    }
+    if (slideIndex < slides.length) {
+      slides[slideIndex].classList.add('next');
+    } else {
+      slides[0].classList.add('next');
+    }
+  }
+  
